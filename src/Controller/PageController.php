7735dce3176f92b774503cec8d92c87310d8b517
Dmitry <ferre1812@gmail.com>
2020-01-07 23:52:04 +0400

    add form

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,13 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Enquiry;
+use App\Form\EnquiryFormType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="homepage", methods={"GET"})
      */
     public function index()
     {
@@ -21,5 +24,24 @@ class PageController extends AbstractController
     public function about()
     {
         return $this->render('page/about.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact_page", methods={"GET", "POST"})
+     */
+    public function contact(Request $request)
+    {
+        $enquiry = new Enquiry();
+
+        $form = $this->createForm(EnquiryFormType::class, $enquiry);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            return $this->redirectToRoute('contact_page');
+        }
+
+        return $this->render('page/contact.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 }
---
+++ src/Entity/Enquiry.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\EnquiryRepository")
+ */
+class Enquiry
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     * @Assert\NotBlank(message="Please, introduce yourself")
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\Email(message="This email is invalid. Please, type correct email")
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     * @Assert\Length(max="50", maxMessage="To long! Topic must be less than 50 char")
+     */
+    private $topic;
+
+    /**
+     * @ORM\Column(type="text", length=300)
+     * @Assert\Length(min="50", max="300")
+     */
+    private $message;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getTopic(): ?string
+    {
+        return $this->topic;
+    }
+
+    public function setTopic(string $topic): self
+    {
+        $this->topic = $topic;
+
+        return $this;
+    }
+
+    public function getMessage(): ?text
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
