48b64788a815ef5bb2bfad3d0f9d0055b678d127
Dmitry <ferre1812@gmail.com>
2020-01-09 10:43:16 +0400

    edit Enquiry & .gitignore

--- src/Entity/Enquiry.php
+++ src/Entity/Enquiry.php
@@ -2,49 +2,30 @@
 
 namespace App\Entity;
 
-use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
-/**
- * @ORM\Entity(repositoryClass="App\Repository\EnquiryRepository")
- */
+
 class Enquiry
 {
     /**
-     * @ORM\Id()
-     * @ORM\GeneratedValue()
-     * @ORM\Column(type="integer")
-     */
-    private $id;
-
-    /**
-     * @ORM\Column(type="string", length=32)
      * @Assert\NotBlank(message="Please, introduce yourself")
      */
     private $name;
 
     /**
-     * @ORM\Column(type="string", length=255)
      * @Assert\Email(message="This email is invalid. Please, type correct email")
      */
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=50)
      * @Assert\Length(max="50", maxMessage="To long! Topic must be less than 50 char")
      */
     private $topic;
 
     /**
-     * @ORM\Column(type="text", length=300)
      * @Assert\Length(min="50", max="300")
      */
     private $message;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 
     public function getName(): ?string
     {
